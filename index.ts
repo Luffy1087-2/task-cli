07e16067b881ef1ac10f1484042b378ddd33c459
Francesco <francesco-lion@virgilio.it>
2025-10-16 14:27:23 +0200

    chore: useless child_process

--- index.ts
+++ index.ts
@@ -1,14 +1,2 @@
 #!/usr/bin/env node
-import { exec } from 'child_process';
-
-const params = process.argv.slice(2).join(" ");
-exec(`npm run task-cli ${params}`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Errore: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Stderr: ${stderr}`);
-  }
-  console.log(`Output: ${stdout}`);
-});
+import './src/index.js';
